a0cc7e721a6d890009f0f9d806c5cbbaece65093
Gabriel Lopez <gabrielbesmonte.lopez@bicol-u.edu.ph>
2025-07-30 11:10:05 +0800

    Mount NAS

--- server/tenDayData/uploadWind.js
+++ server/tenDayData/uploadWind.js
@@ -123,8 +123,9 @@ const mergeTif = async (src1, src2, outputFileName) => {
 };
 
 export const uploadForecastWind = async (year, month, day) => {
-  const SOURCE_PATH = 'C:\\Users\\gabri\\TenDay\\Data';
+  //const SOURCE_PATH = 'C:\\Users\\gabri\\TenDay\\Data';
   //const SOURCE_PATH = '\\\\10.10.3.118\\climps\\10_Day\\Data';
+  const SOURCE_PATH = "/mnt/climps/10_Day/Data";
   const BUCKET_NAME = 'tendayforecast';
 
   const processFolder = async (year, month, day) => {
